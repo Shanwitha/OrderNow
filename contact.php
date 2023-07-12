f1da23ca23afa0574aa2d14c51ef22f81f3c1b26
Shanwitha <106903461+Shanwitha@users.noreply.github.com>
2023-07-12 20:53:59 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php include('partials_front/menu.php') ?>
+
+    <section class="Navigation Bar">
+        <div class="container text-centre">
+            <h2>Contact Shanwitha</h2>
+            <h3>Call 7032790424</h3>
+
+        </div>
+    </section>
+    
+</body>
+</html>
